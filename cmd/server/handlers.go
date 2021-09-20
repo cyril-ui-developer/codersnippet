da6cebe633bac1892f7826f69a9ab73ed5f3240d
Cyril Ajieh <>
2021-09-20 16:30:12 -0400

    add middleware for req log, error recovery and http headers

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -16,6 +16,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
+    panic("oops! something went wrong") 
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
